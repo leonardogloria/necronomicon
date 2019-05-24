5effd1a558b92d76382d882f225dcaf3bd18e08f
Leonardo Silva - Desenvolvimento <lsilva@credilink.com.br>
2019-05-24 11:57:25 -0300

    state of classes

--- lib/model/class.dart
+++ lib/model/class.dart
@@ -1,6 +1,8 @@
 class Class {
   String title;
   int semester;
+  String days;
+  double faults;
   Class(
-  {this.title, this.semester});
+  {this.title, this.semester, this.days, this.faults});
 }
--- lib/tabs/classes.dart
+++ lib/tabs/classes.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:necronomicon/model/class.dart';
 
-class Classes extends StatelessWidget {
-  final makeBody = Container(
-    child: ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: 2,
-      itemBuilder: (BuildContext context, int index) {
-        return makeCard;
-      },
-    ),
-  );
-  static final makeCard = Card(
-    elevation: 8.0,
-    margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-    child: Container(
-      decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-      child: makeListTile,
-    ),
-  );
-  static final makeListTile = ListTile(
-      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+class Classes extends StatefulWidget{
+  @override
+  _Classes createState() => _Classes();
+
+}
+
+class _Classes extends State<Classes> {
+  List classes;
+  @override
+  void initState() {
+    classes = getClasses();
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+
+    ListTile makeListTile(Class _class) => ListTile(
+      contentPadding:
+      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
       leading: Container(
         padding: EdgeInsets.only(right: 12.0),
         decoration: new BoxDecoration(
@@ -29,28 +30,84 @@ class Classes extends StatelessWidget {
         child: Icon(Icons.autorenew, color: Colors.white),
       ),
       title: Text(
-        "Introduction to Driving",
+        _class.title,
         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       ),
       // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
 
       subtitle: Row(
         children: <Widget>[
-          Icon(Icons.linear_scale, color: Colors.yellowAccent),
-          Text(" Intermediate", style: TextStyle(color: Colors.white))
+          Expanded(
+              flex: 1,
+              child: Container(
+                // tag: 'hero',
+                child: LinearProgressIndicator(
+                    backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
+                    value: _class.faults.toDouble(),
+                    valueColor: AlwaysStoppedAnimation(Colors.red)),
+              )),
+          Expanded(
+            flex: 4,
+            child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(_class.days,
+                    style: TextStyle(color: Colors.white))),
+          )
         ],
       ),
       trailing:
-      Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+      Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+
+    );
+
+    Card makeCard(Class _class) => Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        child: makeListTile(_class),
+      ),
+    );
+    final makeBody = Container(
+      // decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: classes.length,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard(classes[index]);
+        },
+      ),
+    );
+    return Scaffold(
+      body: makeBody,
+    );
 
 
 
-  @override
-  Widget build(BuildContext context){
-      return new Scaffold(
-        body: makeBody,
-
-      );
   }
+   List getClasses(){
+    return [
+      Class(
+        title: "Data Sctructure",
+        semester: 2,
+        days: "Mon",
+        faults: 0.3
+      ),
+      Class(
+        title: "Algorithm",
+        semester: 2,
+        days: "Fry",
+        faults: 0
 
+      ),
+      Class(
+        title: "Networks",
+        semester: 2,
+        days: 'Mon - Tue',
+        faults: 0.7
+
+      )
+    ];
+  }
 }
